ccca5c3a6b4892b9d20b23b5ab65f8c765e760e5
강용석 <kangyongsuek@gmail.com>
2018-11-20 14:45:30 +0900

    Create OpenAPI uses with XML

---
+++ precache-manifest.e09738f8fec9470a829e49dc790605ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeb659b13e4c8f1d82a7",
+    "url": "/openAPI_shop/static/js/runtime~main.eeb659b1.js"
+  },
+  {
+    "revision": "2a2b1dce5c90f28d74a0",
+    "url": "/openAPI_shop/static/js/main.2a2b1dce.chunk.js"
+  },
+  {
+    "revision": "4b8da0748f006da760de",
+    "url": "/openAPI_shop/static/js/1.4b8da074.chunk.js"
+  },
+  {
+    "revision": "2a2b1dce5c90f28d74a0",
+    "url": "/openAPI_shop/static/css/main.a4ffa66d.chunk.css"
+  },
+  {
+    "revision": "8e95f01a2a60e5aea90860f7632fd8e0",
+    "url": "/openAPI_shop/index.html"
+  }
+];
